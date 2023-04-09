703110717661cd87102d1fc6bcd29a7c21d83571
Aleksei Tcelishchev <casualuser@users.noreply.github.com>
2023-04-09 16:41:03 -0500

    updated version

--- django_passbook/settings.py
+++ django_passbook/settings.py
@@ -2,3 +2,5 @@ from django.conf import settings
 
 PASSBOOK_CERT = getattr(settings, 'PASSBOOK_CERT', '')
 PASSBOOK_CERT_KEY = getattr(settings, 'PASSBOOK_CERT_KEY', '')
+PASSBOOK_CERT_PASS = getattr(settings, 'PASSBOOK_CERT_PASS', '')
+PASSBOOK_CHAIN_CERT = getattr(settings, 'PASSBOOK_CHAIN_CERT', '')
