df5dbdc684993e35036624d6f18f9b7881b48003
Aleksei Tcelishchev <casualuser@users.noreply.github.com>
2024-03-13 22:40:25 -0500

    Update setup.py with refs to migrations install

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ version = __import__('django_passbook').__version__
 
 setup(
     name='django-passbook',
-    version=version,
-    author='Fernando Aramendi',
-    author_email='fernando@devartis.com',
-    packages=['django_passbook', ],
-    url='http://github.com/devartis/django-passbook/',
+    version='1.0.1',
+    author='Aleksei Tcelishchev',
+    author_email='casualuser@gmail.com',
+    packages=['django_passbook', 'django_passbook.migrations'],
+    url='http://github.com/casualuser/django-passbook/',
     license=open('LICENSE.txt').read(),
-    description='Django Passbook server app',
+    description='Django Passbook server app ',
     long_description=open('README.md').read(),
 
     download_url='http://pypi.python.org/packages/source/D/django-passbook/django-passbook-%s.tar.gz' % version,
@@ -19,8 +19,8 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Other Environment',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
